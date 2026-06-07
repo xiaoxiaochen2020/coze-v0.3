62802e0eb62f0e060129485dd6e77aa98e12b58d
xiaoxiaochen2020 <576877716@qq.com>
2026-06-07 13:51:15 +0800

    [zero-op test] trigger via pending_trigger

---
+++ claw_bridge/pending_trigger/zero_op_test_2026_06_07.bat
@@ -0,0 +1,8 @@
+@echo off
+chcp 437 >nul
+echo === watcher zero-op test 2026-06-07 13:51 ===
+echo HOSTNAME=%COMPUTERNAME%
+echo USER=%USERNAME%
+echo TIME=%DATE% %TIME%
+echo TRIGGER=pending_trigger
+echo. echo === END ===
